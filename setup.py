0210d1c66faf6236815a7d9cd9a146fd35d3d731
Yang Hanlin <mattoncis@hotmail.com>
2021-05-19 01:58:34 +0800

    fix: fix some potential issues in setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ try:
     from setuptools import setup
 except ImportError:
     print('Error: this package requires setuptools to install; try `pip install setuptools`', file=sys.stderr)
-    sys.exit()
+    sys.exit(1)
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 
 
 def get_metadata() -> dict:
     metadata = {}
-    with open('metadata.json', 'r') as metadata_file:
+    with open(os.path.join(script_path, 'metadata.json'), 'r') as metadata_file:
         metadata.update(json.load(metadata_file))
-    with open('requirements.txt', 'r') as requirements_file:
+    with open(os.path.join(script_path, 'requirements.txt'), 'r') as requirements_file:
         # Parses only trivial requirements.txt
         requirements = []
         for line in requirements_file:
