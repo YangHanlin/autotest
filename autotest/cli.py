5d9685d25d874e95c1069dc1592b4876ec12ac11
Yang Hanlin <mattoncis@hotmail.com>
2021-06-04 22:48:31 +0800

    feat: add development mode

--- autotest/cli.py
+++ autotest/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import os
 import logging
 
 from . import run, generate
@@ -14,6 +15,8 @@ available_root_args = [
     '--help',
 ]
 default_action = actions[0]
+
+logger = logging.getLogger(__name__)
 
 
 def init_application() -> None:
@@ -53,7 +56,20 @@ def dispatch_action(args: dict) -> None:
 
 def main() -> None:
     init_application()
-    if len(sys.argv) < 2 or not preflight_check_action(sys.argv[1]):
-        sys.argv.insert(1, default_action.name)
-    args = vars(init_commandline_parser().parse_args())
+    raw_args = sys.argv[1:]
+    if 'AUTOTEST_DEVELOPMENT' in os.environ:
+        logger.info('development mode has been enabled')
+        while True:
+            logger.info('current args ({}) are: {}; enter to continue or type new args'.format(len(raw_args), ', '.join(repr(raw_arg) for raw_arg in raw_args)))
+            new_command = input('>')
+            if not new_command.strip():
+                break
+            raw_args = new_command.split()
+    if not raw_args or not preflight_check_action(raw_args[0]):
+        raw_args.insert(0, default_action.name)
+    args = vars(init_commandline_parser().parse_args(raw_args))
     dispatch_action(args)
+
+
+if __name__ == '__main__':
+    main()
